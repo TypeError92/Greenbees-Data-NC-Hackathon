5b1d1fbd22e14aed57e51080a90cb31234fd9f3b
typeerror92 <simonwalgenbach@posteo.de>
2023-11-25 17:14:02 +0000

    add GET /api endpoint

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+from endpoints import endpoints
 from flask import Flask
 
 app = Flask(__name__)
+
+# ENDPOINT
+
+
+@app.route('/api')
+def healthcheck():
+    return endpoints
+
 
 if __name__ == '__main__':
     app.run()
---
+++ endpoints.py
@@ -0,0 +1,10 @@
+endpoints = {
+
+    # /API
+
+    "GET /api": {
+        "description": "Serves up a json representation of all available endpoints.",
+        "access": ["user", "app", "admin"],
+        "queries": {}
+    }
+}
