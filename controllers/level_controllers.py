d7c1a1d3f1d6f42aa1819c8ec5d5997ab9820a20
typeerror92 <simonwalgenbach@posteo.de>
2023-11-26 17:16:30 +0000

    update level columns

--- controllers/level_controllers.py
+++ controllers/level_controllers.py
@@ -2,10 +2,12 @@ from models.level_models import fetch_level
 
 
 def get_level(level_id):
-    level_id, layout, optimal_solution = fetch_level(level_id)
+    level_id, layout, start, finish, optimal_solution = fetch_level(level_id)
     level = {
         'level_id': level_id,
         'layout': layout,
+        'start': start,
+        'finish': finish,
         'optimal_solution': optimal_solution,
     }
 
